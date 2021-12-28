21ad020bdfef12c2896c40cfe47707bcb777c18c
miriamMartinezUB <31921873+miriamMartinezUB@users.noreply.github.com>
2021-12-28 21:07:56 +0100

    Create and add contants file

---
+++ lib/resources/constants.dart
@@ -0,0 +1,7 @@
+const String regExpPattern = r'\[([^\]]+)\]\(([^\s\)]+)\)';
+
+const String BOLD = 'B';
+const String ITALIC = 'I';
+const String COLORED = 'C';
+const String UNDERLINED = 'U';
+const String LINE_THROUGH = 'T';
